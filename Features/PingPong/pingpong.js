fd6c7b4b47df22a99b87e7ba5af647fca4065e21
Mat <letesteurdelepique@gmail.com>
2021-02-01 20:10:43 +0100

    Ping pong!

---
+++ Features/PingPong/pingpong.js
@@ -0,0 +1,20 @@
+function CheckPingPong(bot, channelID, message) {
+
+    var args = message.substring(1).split(' ');
+    var cmd = args[0];
+
+    args = args.splice(1);
+
+    switch (cmd) {
+        // !ping
+        case 'ping':
+            bot.sendMessage({
+                to: channelID,
+                message: 'Pong!'
+            });
+            return true;
+            break;
+    }
+}
+
+module.exports = { CheckPingPong }
