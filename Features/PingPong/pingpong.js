771f8fa8ada026905046c6bef0763a649d97431f
Mat <letesteurdelepique@gmail.com>
2021-02-04 12:04:41 +0100

    Commented the code

--- Features/PingPong/pingpong.js
+++ Features/PingPong/pingpong.js
@@ -1,19 +1,25 @@
 function CheckPingPong(bot, channelID, message) {
 
+    // The arguments from the command sent
     var args = message.substring(1).split(' ');
+
+    // Storing the first argument
     var cmd = args[0];
 
+    // Removing the first character
     args = args.splice(1);
 
     switch (cmd) {
-        // !ping
+        // !ping command
         case 'ping':
+            // Sends a message
             bot.sendMessage({
                 to: channelID,
                 message: 'Pong!'
             });
-            return true;
+            return true; // Function stops here
     }
 }
 
+// Can be used from the outside
 module.exports = { CheckPingPong }
--- afterlife.js
+++ afterlife.js
@@ -1,35 +1,36 @@
+//#region Includes
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
 
-// My features
+// Our features
 var PingPong = require('./Features/PingPong/pingpong');
+//#endregion
 
-// Configure logger settings
+//#region Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
+//#endregion
 
-// Initialize Discord Bot
+//#region Initialize Discord Bot
 var bot = new Discord.Client({
     token: auth.token,
     autorun: true
 });
+//#endregion
 
-/////////
-
-// Start
+//#region Start
 bot.on('ready', function (evt) {
     // logger.info('Connected');
     // logger.info('Logged in as: ');
     // logger.info(bot.username + ' - (' + bot.id + ')');
 });
+//#endregion
 
-/////////
-
-// When message sent
+//#region On Message event
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
@@ -39,3 +40,4 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 
     }
 });
+//#endregion
